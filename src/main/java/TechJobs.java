4499c80851cefc4789e0c152bbbc10182881860e
Ellen Jardon <e@ellens-mbp.lan>
2023-08-30 22:19:47 -0500

    completed task 2

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -117,7 +117,6 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
         if (someJobs.isEmpty()) {
             System.out.print("No Results");
         } else {
